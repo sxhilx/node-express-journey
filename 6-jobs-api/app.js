3b84e2a040b818d9355b8c2bece0b87d53a7ec8a
sxhilx <jadasahil786@gmail.com>
2025-02-10 17:50:36 +0200

    regestration route setup

--- 6-jobs-api/app.js
+++ 6-jobs-api/app.js
@@ -4,8 +4,13 @@ require('express-async-errors');
 const express = require('express')
 const app = express()
 
+const connectDB = require('./db/connect')
 
+//routers
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
 
+// error handlers
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
@@ -13,9 +18,8 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(express.json())
 
-app.get('/', (req,res) => {
-    res.send('jobs api')
-})
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
@@ -25,6 +29,7 @@ const port = process.env.PORT || 3000
 
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening on port ${port}`))
     } catch (error) {
         console.log(error);
---
+++ 6-jobs-api/modles/User.js
@@ -0,0 +1,30 @@
+const { required } = require('joi')
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true, 'Please provide name'],
+        minlength:3,
+        maxlength:20
+    },
+    email:{
+        type:String,
+        required:[true, 'Please provide email'],
+        match:[/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g, 'Please provide a valid email'],
+        unique:true
+    },
+    password:{
+        type: String,
+        required: [true, 'Please provide password'],
+        minlength: 6
+    }
+})
+
+UserSchema.pre('save', async function() {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+module.exports = mongoose.model(`User`, UserSchema)
