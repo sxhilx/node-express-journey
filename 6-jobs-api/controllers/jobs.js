208fdeb1e560ad228cbdb9382cef7a8b47555bf8
sxhilx <jadasahil786@gmail.com>
2025-02-16 13:25:57 +0200

    Delete functionality created

--- 6-jobs-api/controllers/jobs.js
+++ 6-jobs-api/controllers/jobs.js
@@ -58,7 +58,20 @@ const updateJob = async (req, res) => {
 }
 
 const deleteJob = async (req, res) => {
-    res.send('delete job')
+    const {
+        user:{userId}, 
+        params:{id:jobId}
+    } = req
+
+    const job = await Job.findByIdAndDelete({
+        _id:jobId, createdBy:userId
+    })
+
+    if(!job){
+        throw new NotFoundError(`No job with the id: ${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).send()
 }
 
 module.exports = {
